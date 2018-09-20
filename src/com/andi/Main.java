ff65d074cacefd2dd3f05ae50a6ae729290b5d67
andi <amayer80@gmail.com>
2018-09-20 12:08:48 +0200

    second version

--- src/com/andi/Main.java
+++ src/com/andi/Main.java
@@ -8,7 +8,20 @@ public class Main {
 
     public static void main(String[] args) {
         testingObjectAdapter();
+        testingClassAdapter();
 
+    }
+
+    private static void testingClassAdapter() {
+        SocketAdapter socketAdapter = new SocketObjectAdapterImplementation();
+        Volt v3 = getVolt(socketAdapter, 3);
+        Volt v12 = getVolt(socketAdapter, 12);
+
+        Volt v120 = getVolt(socketAdapter, 120);
+
+        System.out.println("V3 volts is using Class Adapter " +v3.getVolts());
+        System.out.println("V12 volts is using Class Adapter " +v12.getVolts());
+        System.out.println("V120 volts is using Class Adapter " +v120.getVolts());
     }
 
     private static void testingObjectAdapter() {
